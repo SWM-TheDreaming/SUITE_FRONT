abb654c78610f112e595b8bd030ffa34c8ed68f2
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-07-28 09:50:33 +0900

    feat: SafeAreaProvider setting

--- SUITE/src/screens/Studylist.tsx
+++ SUITE/src/screens/Studylist.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import StudyInfoCard from '../components/containers/studyInfoCard.container';
+import mainPageStyleSheet from '../style/style';
 
 const Studylist = () => {
   return (
-    <View>
-      <Text>studylist page</Text>
+    <View style={mainPageStyleSheet.container}>
+      <StudyInfoCard />
     </View>
   );
 };
