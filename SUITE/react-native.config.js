0d231652a05d5d314282049ec942c7f8bc807455
yujeonghyeop <one8775@ajou.ac.kr>
2023-10-23 14:10:05 +0900

    fix: setting change

--- SUITE/react-native.config.js
+++ SUITE/react-native.config.js
@@ -4,7 +4,7 @@ module.exports = {
     android: {},
   },
   assets: ['./assets/fonts/'],
-  API_URL: 'http://test.suitestudy.com',
-  API_URL_SUITEROOM: 'http://test.suitestudy.com',
-  API_URL_STUDYROOM: 'http://test.suitestudy.com',
+  API_URL: 'http://api.suitestudy.com',
+  API_URL_SUITEROOM: 'http://api.suitestudy.com',
+  API_URL_STUDYROOM: 'http://api.suitestudy.com',
 };
